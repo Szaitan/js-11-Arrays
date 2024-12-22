1b4aed8bf0ef4ff1c429ab1da83051bf02ac4e9a
Michal <m.krepiniewicz@gmail.com>
2024-12-22 14:34:53 +0100

    challenge 1 complete

---
+++ challenge.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function checkDogs(dogsJulia, dogsKatie) {
+  const correctDogsJulia = dogsJulia.slice(1, dogsJulia.length - 2);
+  console.log(correctDogsJulia);
+
+  const combineDogsData = correctDogsJulia.concat(dogsKatie);
+  console.log(combineDogsData);
+
+  combineDogsData.forEach(function (age, i) {
+    age > 3
+      ? console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`)
+      : console.log(`Dog number ${i + 1} is still a puppy`);
+  });
+
+  for (let [i, age] of combineDogsData.entries()) {
+    age > 3
+      ? console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`)
+      : console.log(`Dog number ${i + 1} is still a puppy`);
+  }
+}
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
