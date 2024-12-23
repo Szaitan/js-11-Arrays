35bbd640a2f34efd5e37b21c2684acf54bb3b96e
Michal <m.krepiniewicz@gmail.com>
2024-12-23 21:26:54 +0100

    filter array

--- filter-array.js
+++ filter-array.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Filter return us new array which containes valye which pass the if statment
+// (without if word)
+// Callback function can also take value, index and array
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposit = movements.filter(function (value) {
+  return value > 0;
+});
+console.log(deposit);
+
+// Using arrow function
+const withdraw = movements.filter(value => value < 0);
+console.log(withdraw);
