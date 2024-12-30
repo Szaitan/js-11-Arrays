4bdaace80403c2ffc52b8982755d79e7252af551
Michal <m.krepiniewicz@gmail.com>
2024-12-30 11:46:57 +0100

    Challenge 4 and 5

--- challenge.js
+++ challenge.js
@@ -55,3 +55,81 @@ function calcAverageHumanAge2(arr) {
   return avgFinal;
 }
 console.log(calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]));
+
+// Challenge 4
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// part 1
+for (const dog of dogs) {
+  dog.recommendedFood = Math.floor(dog.weight ** 0.75 * 28);
+  console.log(dog.recommendedFood);
+}
+
+// part 2
+const sarahDog = dogs.find(function (dog) {
+  return dog.owners.includes('Sarah');
+});
+const sarahPerc10DogsFood = sarahDog.recommendedFood * 0.1;
+
+console.log(
+  `Sarah dog eats ${
+    sarahDog.curFood > sarahDog.recommendedFood ? 'to much' : 'to little'
+  }`
+);
+
+// part 3
+const ownersEatTooMuch = dogs
+  .filter(function (dog) {
+    return dog.recommendedFood < dog.curFood;
+  })
+  .map(function (dog) {
+    return dog.owners;
+  })
+  .flat();
+
+const ownersEatTooLittle = dogs
+  .filter(function (dog) {
+    return dog.recommendedFood > dog.curFood;
+  })
+  .map(function (dog) {
+    return dog.owners;
+  })
+  .flat();
+
+console.log(ownersEatTooMuch);
+console.log(ownersEatTooLittle);
+
+// part 4
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// part 5
+console.log(
+  dogs.some(function (dog) {
+    dog.curFood === dog.recommendedFood;
+  })
+);
+
+// part 6
+dogs.forEach(function (dog) {
+  console.log(
+    dog.curFood > dog.recommendedFood * 0.9 &&
+      dog.curFood < dog.recommendedFood * 1.1
+  );
+});
+
+// part 7
+const theTrueDog = dogs.filter(function (dog) {
+  return (
+    dog.curFood > dog.recommendedFood * 0.9 &&
+    dog.curFood < dog.recommendedFood * 1.1
+  );
+});
+console.log(theTrueDog);
+
+// part 8
