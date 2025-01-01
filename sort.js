6a895e4d4159e3eff2e1870cdc49285db373403f
Michal <m.krepiniewicz@gmail.com>
2025-01-01 15:37:06 +0100

    sort array

---
+++ sort.js
@@ -0,0 +1,27 @@
+// Sort method alows to sort array
+// It can sort strings from lower to higher
+// IMPORTANT: It mutates exisiting array
+const arr1 = ['mike', 'Adam'];
+console.log(arr1.sort()); //[ "Adam", "mike" ]
+
+// However for array with number we have to pass callback function
+
+// Why it works?
+// It works like this becasuse if a - b gives number less then 0
+// then a should be infront of b, if a - b is positive b should be
+// infront of a
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// Ascending order
+console.log(
+  movements.sort(function (a, b) {
+    return a - b;
+  })
+);
+
+// Descending order
+console.log(
+  movements.sort(function (a, b) {
+    return a + b;
+  })
+);
